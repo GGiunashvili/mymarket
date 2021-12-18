be646865048dad66000768dfbc79cbb26060aea9
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2021-12-18 20:47:08 +0400

    add search more title slider arrows

--- javascript/custom.js
+++ javascript/custom.js
@@ -41,6 +41,10 @@ var swiper = new Swiper(".mySwiper-banners", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    navigation: {
+      nextEl: ".search-more__slider__box__button-next",
+      prevEl: ".search-more__slider__box__button-prev",
+    },
      breakpoints: {
         // when window width is >= 480px
         768: {
@@ -51,6 +55,33 @@ var swiper = new Swiper(".mySwiper-banners", {
         992: {
           slidesPerView: 3,
           spaceBetween: 20,
+
+        }
+     },
+  });
+
+
+
+
+
+  var swiper = new Swiper(".search-more__slider__box__title-box__component__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 8,
+ 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+     breakpoints: {
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 8,
+        },
+        // when window width is >= 640px
+        992: {
+          slidesPerView: 6,
+          spaceBetween: 8,
 
         }
      },
@@ -164,7 +195,7 @@ var swiper = new Swiper(".mySwiper-banners", {
 
   var swiper = new Swiper(".recomended__slider__box__title-box__component__swiper", {
     slidesPerView: 2,
-    spaceBetween: 15,
+    spaceBetween: 8,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -177,16 +208,16 @@ var swiper = new Swiper(".mySwiper-banners", {
         // when window width is >= 480px
         576: {
           slidesPerView: 3,
-          spaceBetween: 15,
+          spaceBetween: 8,
         },
         768: {
           slidesPerView: 3,
-          spaceBetween: 27,
+          spaceBetween: 8,
         },
         // when window width is >= 640px
         1200: {
           slidesPerView: 6,
-          spaceBetween: 12,
+          spaceBetween: 8,
 
         }
      },
